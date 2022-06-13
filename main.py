e6b632165d07710c7a2a34369d0f830e3826852f
eduardo-moro <eduardomoro1127@gmail.com>
2022-06-13 10:40:55 -0300

    Adicionado pacote Textual, este módulo está responsável pelo scroll, toque, atalhos e outras funcoes de TUI do programa.
    Correcao nescessaria!!!
    Es5ou modificando a forma como as noticias sao mostradas, implementando
    o markdown do modulo rich, tambem é nescessario corrigir o formato da
    table principal

--- main.py
+++ main.py
@@ -1,64 +1,105 @@
-from rich import pretty, print as rprint
 from rich.panel import Panel as panel
+from rich.table import Table
 from rich.layout import Layout
-from rich.console import Console
-from rich.live import Live
-from rich.console import group, Group
-from rich.text import Text
+from rich.console import Console, RenderableType
+from rich.console import group
+from rich.markdown import Markdown
 from requests import get
-from time import sleep
-
-import json
+from textual.app import App
+from textual.widgets import ScrollView
+from textual.widget import Widget
+from textual.reactive import Reactive
 
 API_URL = "https://www.tabnews.com.br/api/v1/"
-POSTS_PER_PAGE = 4
+POSTS_PER_PAGE = 30
 
-if __name__ == "__main__":    
-    
-    post_page = 1
-    console = Console()
-    layout = Layout()
+post_page = 1
+console = Console()
+layout = Layout()
 
-    def get_url(append):
-        return API_URL + append
 
-    def get_posts(page):
-        response = get(get_url('contents'), params={'per_page': POSTS_PER_PAGE, 'page': page}).json()
-        return response
-    
-    def post_layout(post, padding=0, limit=180): 
-        body = post["body"][:limit]
-        content = f'[bold]{post["title"]}  [/bold]\n[#0969da on #333333] {post["username"]} [/#0969da on #333333]\n\n{body}'
-        
-        return panel(content, padding=padding)
+def get_url(append):
+    return API_URL + append
 
-    def get_first_post():
-        return get_posts(1)[0]
 
-    post_shown = post_layout(get_first_post(), padding=1, limit=None)
+def get_posts(page):
+    response = get(
+        get_url('contents'),
+        params={'per_page': POSTS_PER_PAGE, 'page': page}
+    ).json()
+    return response
 
-    @group()
-    def feed_layout(page):
-        for post in get_posts(page):
-            yield post_layout(post)
 
-    layout.split(
-        Layout(name="header", size=2),
-        Layout(name="body", ratio=1)
-    )
+def post_layout(post, padding=1):
+    content = f'[bold]{post["title"]}  [/bold]\n\
+[#0969da on #333333] {post["username"]} [/#0969da on #333333]'
 
-    layout["header"].split(
-        Layout(Text('TabNews CLI', style="bold")),
-        Layout(Text('Criado por Eduardo-moro [finn]', style="bold cyan", justify="right")),
-        splitter="row"
-    )
+    return panel(content, padding=padding, expand=True)
 
-    layout["body"].split(
-        Layout(post_shown,name="post", ratio=2),
-        Layout(feed_layout(post_page),name="feed"),
-        splitter="row"
-    )
 
-    with Live(layout, screen=True):
-        while True:
-            sleep(0)      
+def get_first_post():
+    return get_posts(1)[0]
+
+
+post_shown = post_layout(get_first_post(), padding=1)
+
+
+@group()
+def feed_layout(page):
+    for post in get_posts(page):
+        yield post_layout(post)
+
+
+class Header(Widget, Table):
+    def render(self) -> None:
+        table = Table.grid(padding=(0, 1), expand=True)
+        table.add_column("app", justify="left", ratio=0, width=10)
+        table.add_column("creator", justify="right", width=20)
+        table.add_row("CliNews", "Criado por Eduardo-moro [finn]")
+
+        header: RenderableType
+        header = panel(table)
+
+
+        return table
+
+
+class Feed(Widget):
+    mouse_over = Reactive(False)
+
+    def render(self) -> panel:
+        return panel(feed_layout(post_page), border_style=("white" if self.mouse_over else "black"))
+
+    def on_enter(self) -> None:
+        self.mouse_over = True
+
+    def on_leave(self) -> None:
+        self.mouse_over = False
+
+
+class CliNews(App):
+    async def on_load(self) -> None:
+        await self.bind('q', 'quit', 'sair')
+
+    async def on_mount(self) -> None:
+        await self.view.dock(Header(), edge="top", size=3)
+        grid = await self.view.dock_grid(edge="left")
+
+        grid.add_column(fraction=3, name="post", min_size=60)
+        grid.add_column(fraction=1, name="feed")
+
+        grid.add_row(fraction=1, name="main")
+        grid.add_row(fraction=2, name="content")
+
+        grid.add_areas(
+            post="post, main",
+            feed="feed, main"
+        )
+
+        grid.place(
+            post=ScrollView(post_shown),
+            feed=ScrollView(Feed())
+        )
+
+
+CliNews.run(log="textual.log")
